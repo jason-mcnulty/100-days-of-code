e17100f4e4030092e65e199f0e153fc25505b3d3
“Jason” <jason.mcnulty@gmail.com>
2021-06-20 16:10:15 -0400

    fixed elif.py

--- day-2/elif.py
+++ day-2/elif.py
@@ -1,15 +1,19 @@
 #elif statements allow you to add more than one else stement
-# THIS NEEDS TO BE FIXED
 print('what is your name? ')
 name = input()
 print('what is your age? ')
 age = input()
+# conveting string from the input into an intiger
+age = int(str(age)) 
 if name == 'jason':
     print('hi jason')
 elif age == 43:
     print('you are ' + str(int(age))) # str(int(age))
-elif age < 50:
+elif age < 50:    
     print ('you are ' + str(int(age)))
 elif name == 'bob':
     print('its really you ' + name)
-print('done')
+# If all values are false, you can use an else statement.
+else :
+    print('Wow that is old.')
+
